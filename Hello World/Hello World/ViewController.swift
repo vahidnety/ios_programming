e97c786dbae2f3dce0111d725a995e86c4278789
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-10-03 16:12:42 -0700

    first test app

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textInputField: UITextField!
+    @IBOutlet weak var labelName: UILabel!
+   
+    @IBAction func submit(_ sender: AnyObject) {
+        
+        labelName.text = textInputField.text
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
