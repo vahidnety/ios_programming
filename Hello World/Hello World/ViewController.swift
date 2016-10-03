e017d2973e56f9c3dd496eeeed74dfb8cf4a5e11
Yeli-Buonya <ybuonya748@southseattle.edu>
2016-10-03 10:51:23 -0700

    initial commit

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Yeli Buonya on 10/3/16.
+//  Copyright © 2016 VetsWhocode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
